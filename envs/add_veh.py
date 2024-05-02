1e041971d6570bf45468bf1002d0e7935828ea8f
SHITIANYU-hue <tianyu.s@outlook.com>
2024-05-02 18:50:00 -0400

    update merge vehicles color and detectors

--- envs/add_veh.py
+++ envs/add_veh.py
@@ -58,7 +58,7 @@ def qew_merge_add_veh(t,mainlane_demand,merge_lane_demand):
 
 
 
-def syn_merge_add_veh(t,mainlane_demand,merge_lane_demand,factor=0.05):
+def syn_merge_add_veh(t,mainlane_demand,merge_lane_demand,factor=0.5):
     hdv_main_='hdvmain_'
     hdv_merge_ = 'hdvmerge_' 
     cav_main_ = 'cavmain_' 
@@ -84,11 +84,11 @@ def syn_merge_add_veh(t,mainlane_demand,merge_lane_demand,factor=0.05):
         traci.vehicle.add(hdv_main, routeID='route_0', typeID='rl', departLane='random', departSpeed=departspeed)
         traci.vehicle.setSpeed(hdv_main, departspeed)
         # traci.vehicle.setSpeedFactor(veh_name, 3)
-        # traci.vehicle.setLaneChangeMode(hdv_main,0b001000000000)
+        traci.vehicle.setLaneChangeMode(hdv_main,0b001000000000)
 
     # Merge lane demand
     if 0 <= t <= factor*18000:
-        inflow_rate_merge_lane = merge_lane_demand *(t/18000)/36000
+        inflow_rate_merge_lane = merge_lane_demand /36000
     elif factor*18000 < t <= factor*54000:
         inflow_rate_merge_lane = merge_lane_demand/36000
     elif factor*54000 < t <= factor*72000:
@@ -102,6 +102,50 @@ def syn_merge_add_veh(t,mainlane_demand,merge_lane_demand,factor=0.05):
     if u_merge_lane < inflow_rate_merge_lane:
         traci.vehicle.add(hdv_merge, routeID='route_1', typeID='rl', departLane='free', departSpeed=departspeed)
         traci.vehicle.setSpeed(hdv_merge, departspeed)
+        # traci.vehicle.setSpeedFactor(veh_name4,3)
+        traci.vehicle.setLaneChangeMode(hdv_merge,0b001000000000)
+
+    veh_id_list=list(traci.vehicle.getIDList())
+
+    return veh_id_list
+
+def syn_merge_add_veh_constant(t,mainlane_demand,merge_lane_demand,factor=0.5):
+    hdv_main_='hdvmain_'
+    hdv_merge_ = 'hdvmerge_' 
+    cav_main_ = 'cavmain_' 
+    cav_merge_ = 'cavmerge_' 
+    hdv_main=hdv_main_+str(t)
+    hdv_merge=hdv_merge_+str(t)
+
+    departspeed = random.choice([27, 30])
+    # speed_mode=32
+    # Mainline demand
+    inflow_rate_mainline = mainlane_demand /3600
+
+    # print('inflow_rate_mainline',inflow_rate_mainline)
+    
+    # Sample from a uniform distribution for mainline
+    u_mainline = np.random.uniform(0, 1)
+    # Check if a vehicle should be generated based on the sampled value for mainline
+    if u_mainline < inflow_rate_mainline:
+        traci.vehicle.add(hdv_main, routeID='route_0', typeID='rl', departLane='random', departSpeed=departspeed)
+        traci.vehicle.setColor(hdv_main,[0,255,255,255])
+        traci.vehicle.setSpeed(hdv_main, departspeed)
+        
+        # traci.vehicle.setSpeedFactor(veh_name, 3)
+        # traci.vehicle.setLaneChangeMode(hdv_main,0b001000000000)
+
+    # Merge lane demand
+    inflow_rate_merge_lane = merge_lane_demand /3600
+
+    # print('inflow_rate_mergeline',inflow_rate_merge_lane)
+    # Sample from a uniform distribution for merge lane
+    u_merge_lane = np.random.uniform(0, 1)
+    # Check if a vehicle should be generated based on the sampled value for merge lane
+    if u_merge_lane < inflow_rate_merge_lane:
+        traci.vehicle.add(hdv_merge, routeID='route_1', typeID='rl', departLane='free', departSpeed=departspeed)
+        traci.vehicle.setSpeed(hdv_merge, departspeed)
+        traci.vehicle.setColor(hdv_merge,[255,0,0,255])
         # traci.vehicle.setSpeedFactor(veh_name4,3)
         # traci.vehicle.setLaneChangeMode(hdv_merge,0b001000000000)
 
